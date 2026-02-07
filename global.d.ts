8918bb90326ce188a89e400feb5acb3fddcce1b3
CONCEPTFAB <office@conceptfab.com>
2026-02-07 16:17:06 +0100

    b 0.36C: C

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,16 @@ declare module "*.json" {
   const value: unknown;
   export default value;
 }
+
+declare module 'adm-zip' {
+  interface ZipEntry {
+    entryName: string;
+    isDirectory: boolean;
+    getData(): Buffer;
+  }
+  class AdmZip {
+    constructor(buffer?: Buffer);
+    getEntries(): ZipEntry[];
+  }
+  export = AdmZip;
+}
